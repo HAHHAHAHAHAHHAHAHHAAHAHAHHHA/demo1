88ca123eb23b462b5e9dceded4f6e1c1f45bd710
zhaoshiqi <2503642089@qq.com>
2022-09-01 14:02:16 +0800

    这是提交记录2

--- src/main/java/com/atugigu/git/Hello.java
+++ src/main/java/com/atugigu/git/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
         System.out.println("hello");
         System.out.println("hello1");
         System.out.println("这个代码新增,推送到GitHub中");
+        System.out.println("这个代码第二次新增,推送到GitHub中");
     }
 }
